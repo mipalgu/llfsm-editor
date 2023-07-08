8dd9b57590c8585b1e30d6761931cae059071f8b
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-07-08 17:19:43 +1000

    Trying to add new element to array.

--- src/Settings.js
+++ src/Settings.js
@@ -5,16 +5,16 @@ function Row(initialExternal) {
     
     const onChangeName = useCallback((evt) => {
         setExternal({ ...external, name: evt.target.value });
-    });
+    }, [external, setExternal]);
     const onChangeType = useCallback((evt) => {
         setExternal({ ...external, type: evt.target.value });
-    });
+    }, [external, setExternal]);
     const onChangeMode = useCallback((evt) => {
         setExternal({ ...external, mode: evt.target.value });
-    });
+    }, [external, setExternal]);
     const onChangeValue = useCallback((evt) => {
         setExternal({ ...external, defaultValue: evt.target.value });
-    });
+    }, [external, setExternal]);
     
     return (
         <tr>
@@ -22,12 +22,45 @@ function Row(initialExternal) {
             <td><input type="text" onChange={onChangeType} value={external.type} /></td>
             <td><input type="text" onChange={onChangeMode} value={external.mode} /></td>
             <td><input type="text" onChange={onChangeValue} value={external.defaultValue} /></td>
+            <td></td>
         </tr>
     );
 }
 
 function Settings(machine) {
     const [externals, setExternals] = useState(machine.externalVariables ?? []);
+    const [newName, setNewName] = useState('');
+    const [newType, setNewType] = useState('');
+    const [newMode, setNewMode] = useState('');
+    const [newValue, setNewValue] = useState('');
+
+    const onSubmit = useCallback((evt) => {
+        evt.preventDefault();
+        setExternals(externals.concat({
+            name: newName, type: newType, mode: newMode, defaultValue: newValue
+        }));
+        setNewName('');
+        setNewType('');
+        setNewMode('');
+        setNewValue('');
+    },
+        [
+            externals, setExternals, newName, setNewName, newType, setNewType, newMode, setNewMode, newValue,
+            setNewValue
+        ]
+    );
+    const onChangeName = useCallback((evt) => {
+        setNewName(evt.target.value);
+    }, [setNewName]);
+    const onChangeType = useCallback((evt) => {
+        setNewType(evt.target.value);
+    }, [setNewType]);
+    const onChangeMode = useCallback((evt) => {
+        setNewMode(evt.target.value);
+    }, [setNewMode]);
+    const onChangeValue = useCallback((evt) => {
+        setNewValue(evt.target.value);
+    }, [setNewValue]);
 
     return (
         <div>
@@ -41,12 +74,25 @@ function Settings(machine) {
                 <div>
                     <table>
                         <thead>
-                            <tr><th>Name</th><th>Type</th><th>Mode</th><th>Default Value</th></tr>
+                            <tr><th>Name</th><th>Type</th><th>Mode</th><th>Default Value</th><th></th></tr>
                         </thead>
                         <tbody>
                             {externals.map((external) => Row(external))}
                         </tbody>
                     </table>
+                    <form onSubmit={onSubmit}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><input type="text" onChange={onChangeName} value={newName} /></td>
+                                    <td><input type="text" onChange={onChangeType} value={newType} /></td>
+                                    <td><input type="text" onChange={onChangeMode} value={newMode} /></td>
+                                    <td><input type="text" onChange={onChangeValue} value={newValue} /></td>
+                                    <td><input type="submit" /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </form>
                 </div>
             </section>
         </div>
